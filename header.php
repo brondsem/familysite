4da4124979c96e4e3cb78a90e22a720aaa439368
Dave Brondsema <dave@brondsema.net>
2008-12-20 20:27:02 -0500

    setup check

--- header.php
+++ header.php
@@ -1,4 +1,7 @@
 <?php
+if (!file_exists(dirname(__FILE__).'/config.php')) {
+	die("You need to create a config.php.  Please copy config-example.php and modify to suit your environment.");
+}
 require_once(dirname(__FILE__).'/config.php');
 require_once(dirname(__FILE__).'/arc/ARC2.php');
 require_once(dirname(__FILE__).'/openid.php');
